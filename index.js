aa911119e58d322eabfabdcd5839e2f9b149f0e2
fromleonkariu <leonkariu@outlook.com>
2025-03-18 23:06:26 +0300

    day 1 commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello traveller welcome to KD Studios -dark side 😂😂😂😂👌❌")
+
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+    console.log("Dark mode is now " + (body.classList.contains("dark-mode") ? "on" : "off"));
+});
+
+
+
+
