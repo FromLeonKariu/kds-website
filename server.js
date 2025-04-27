acad7b0784454e0348aa82ede9ea63ec719e722d
fromleonkariu <leonkariu@outlook.com>
2025-04-27 11:21:04 +0300

    updated the structure of the app and mobile resp

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require ("express");
 const app = express();
+const path = require("path");
 
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { res.send('Hello, World!'); });
+
+//Page routing
+
+app.get('/', (req, res) => {
+     res.sendFile(pat.join(__dirname, 'index.html')) 
+    });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
