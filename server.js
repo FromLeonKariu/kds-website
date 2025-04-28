c1fed41e139cefb17645d77b15c5234a7d80c3f7
fromleonkariu <leonkariu@outlook.com>
2025-04-28 22:49:07 +0300

    init

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const path = require("path");
 
@@ -9,9 +9,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Page routing
 
 app.get('/', (req, res) => {
-     res.sendFile(pat.join(__dirname, 'index.html')) 
+     res.sendFile(path.join(__dirname, 'index.html')) 
     });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
+try {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+} catch (error) {
+    console.error("An error occurred while starting the server:", error);
+}
